b08c4780cb2dde85c6a8a16c2b7070621be13650
Tang111111 <weta9784@colorado.edu>
2019-01-31 19:47:29 -0700

    Finished!

---
+++ lab2_scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Author Weiyao Tang
+#Date 01/31/2019
+
+#Problem 1 Code:
+echo "Enter an expression: "
+read expression
+echo "Enter an file name"
+read fname
+grep expression fname
+grep -Ec "[0-9]{3}-[0-9]{3}-[0-9]{4}" regex_practice.txt
+grep -Ec "@" regex_practice.txt
+grep -E   "^303-" regex_practice.txt > ~/Documents/lab_2/phone_results.txt
+grep -E  "@geocities.com$" regex_practice.txt > ~/Documents/lab_2/email_results.txt
+grep -E "$1" regex_practice.txt > ~/Documents/lab_2/command_results.txt
+
